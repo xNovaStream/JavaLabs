e4c9ea1457a692521f79f31091888841035f1ccc
xNovaStream <vlad.cononov2003@yandex.ru>
2024-06-08 15:48:09 +0300

    lab 5 some fixes

--- AccountingCatsAndOwners/OwnerMicroservice/src/main/java/ru/itmo/dao/ICatDao.java
+++ AccountingCatsAndOwners/OwnerMicroservice/src/main/java/ru/itmo/dao/ICatDao.java
@@ -1,10 +1,8 @@
 package ru.itmo.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import ru.itmo.entity.CatColor;
 import ru.itmo.entity.CatEntity;
 
-import java.util.List;
 import java.util.UUID;
 
 public interface ICatDao extends JpaRepository<CatEntity, UUID> {
--- AccountingCatsAndOwners/OwnerMicroservice/src/main/java/ru/itmo/dao/IUserDao.java
+++ AccountingCatsAndOwners/OwnerMicroservice/src/main/java/ru/itmo/dao/IUserDao.java
@@ -7,5 +7,4 @@ import java.util.Optional;
 import java.util.UUID;
 
 public interface IUserDao extends JpaRepository<UserEntity, UUID> {
-    Optional<UserEntity> findByLogin(String login);
 }
