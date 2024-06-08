85e2c17c52da875271e16f9824ed76359dca37d9
xNovaStream <vlad.cononov2003@yandex.ru>
2024-06-08 15:48:36 +0300

    lab 5 also some fixes

--- AccountingCatsAndOwners/OwnerMicroservice/src/main/java/ru/itmo/dao/IUserDao.java
+++ AccountingCatsAndOwners/OwnerMicroservice/src/main/java/ru/itmo/dao/IUserDao.java
@@ -3,7 +3,6 @@ package ru.itmo.dao;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.itmo.entity.UserEntity;
 
-import java.util.Optional;
 import java.util.UUID;
 
 public interface IUserDao extends JpaRepository<UserEntity, UUID> {
